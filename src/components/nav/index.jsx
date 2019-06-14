fe2b7f75ccb3f39806232fd25a6278170fc59207
Timothy Hooker <timothy@ministrycentered.com>
2019-06-14 16:53:14 -0400

    small adjustments to nav

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -9,8 +9,8 @@ function Nav() {
     <nav className="page-nav flex">
       <button href="#" className={`${linkStyle}`}>
         <Mail size={32} className={iconStyle} alt="contact icon" />        
-        <span className="dn dib-l">
-          contact
+        <span className="dn dib-l ttu">
+          Contact
         </span>
       </button>
       <a href="https://www.google.com/maps/place/418+Marshall+St+N,+Winston-Salem,+NC+27101/@36.0992078,-80.2506814,17z/data=!3m1!4b1!4m5!3m4!1s0x8853ae43e3ece7dd:0xe4a51872326dce0c!8m2!3d36.0992078!4d-80.2484927" target="_blank" className={`${linkStyle}`}>
