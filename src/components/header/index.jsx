d9ddc87058317bc11b4245cde39b268820cdd3b1
Timothy Hooker <timothy@ministrycentered.com>
2019-06-16 11:42:31 -0400

    accessibility improvements

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -8,19 +8,19 @@ function Header() {
   return (
     <header className="page-header fixed left-0 top-0 right-0 z-1">
       <nav className="page-nav flex">
-        <button href="#" className={`${linkStyle}`}>
+        <button aria-label="contact us" href="#" className={`${linkStyle}`}>
           <Mail size={32} className={iconStyle} alt="contact icon" />        
           <span className="dn dib-l ttu">
             Contact
           </span>
         </button>
-        <a href="https://www.google.com/maps/place/418+Marshall+St+N,+Winston-Salem,+NC+27101/@36.0992078,-80.2506814,17z/data=!3m1!4b1!4m5!3m4!1s0x8853ae43e3ece7dd:0xe4a51872326dce0c!8m2!3d36.0992078!4d-80.2484927" target="_blank" rel="noopener noreferrer" className={`${linkStyle}`}>
+        <a aria-label="link to google maps for 418 N. Marshall St" href="https://www.google.com/maps/place/418+Marshall+St+N,+Winston-Salem,+NC+27101/@36.0992078,-80.2506814,17z/data=!3m1!4b1!4m5!3m4!1s0x8853ae43e3ece7dd:0xe4a51872326dce0c!8m2!3d36.0992078!4d-80.2484927" target="_blank" rel="noopener noreferrer" className={`${linkStyle}`}>
           <MapPin size={32} className={iconStyle} />
           <span className="dn dib-l">
             418 N. Marshall Street, Winston-Salem, NC 27101
           </span>
         </a>
-        <a href="tel:1-336-831-3723" className={`${linkStyle}`}>
+        <a aria-label="Call 336-831-3723" href="tel:1-336-831-3723" className={`${linkStyle}`}>
           <Phone size={32} className={iconStyle} alt="phone icons" />
           <span className="dn dib-l">
             336.831.3723
