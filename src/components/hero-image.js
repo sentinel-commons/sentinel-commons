649c6d21c0a046bde8466f9a5fa55469ee1259a6
Timothy Hooker <timothy@ministrycentered.com>
2019-06-16 11:30:22 -0400

    fixup! fix hero image

--- src/components/hero-image.js
+++ src/components/hero-image.js
@@ -2,18 +2,6 @@ import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import Media from "react-media"
-// import heroImageMobile from '../images/hero-mobile.jpg'
-
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `StaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `StaticQuery`: https://gatsby.dev/staticquery
- */
 
 const HeroImage = props => (
   <StaticQuery
@@ -37,12 +25,20 @@ const HeroImage = props => (
     `}
     render={data => {
       return (
-        <Media query={{ maxWidth: '60rem' }}>
+        <Media query={{ maxWidth: "60rem" }}>
           {matches =>
             matches ? (
-              <Img className="hero__image" style={{ position: 'absolute' }} fluid={data.mobileHeroImage.childImageSharp.fluid} />
+              <Img
+                className="hero__image"
+                style={{ position: "absolute" }}
+                fluid={data.mobileHeroImage.childImageSharp.fluid}
+              />
             ) : (
-              <Img className="hero__image" style={{ position: 'absolute' }} fluid={data.desktopHeroImage.childImageSharp.fluid} />
+              <Img
+                className="hero__image"
+                style={{ position: "absolute" }}
+                fluid={data.desktopHeroImage.childImageSharp.fluid}
+              />
             )
           }
         </Media>
